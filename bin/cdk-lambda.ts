213af5fa6ec3c461f8826b192ab59c414e59e224
Kamran Maharov <kamrangs@hotmail.com>
2021-12-18 01:31:30 -0800

    check in lambda handler and Function definitions

--- bin/cdk-lambda.ts
+++ bin/cdk-lambda.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { CdkLambdaStack } from '../lib/cdk-lambda-stack';
 
 const app = new cdk.App();
--- lib/cdk-lambda-stack.ts
+++ lib/cdk-lambda-stack.ts
@@ -1,16 +1,17 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
 
-export class CdkLambdaStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CdkLambdaStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const helloWorldFunction = new lambda.Function(this,
+    	"KamransTestLambdaFunction",
+    	{
+    		code: lambda.Code.fromAsset("functions/helloworld"),
+    		handler: "index.handler",
+    		runtime: lambda.Runtime.NODEJS_12_X,
+    		description: "This Lambda Function returns the message: 'Hello World!' in the response body",
+    	});
   }
 }
